bff625f48d133e749bae3cba752379d266d04419
wim (SVN Import) <info@achievo.org>
2001-08-16 08:39:11 +0000

    Added the priority header to the mail messages shows a nice exclamation sign on high prio's

--- appoint.php
+++ appoint.php
@@ -8,9 +8,11 @@
 <html>
 <head>
  <title>Achievo scheduler item</title>
- <? if ($mode == 1) { ?>
- <script language="javascript" src="atk/javascript/open_appointment.js"></script>
- <? } ?>
+ <script language="JavaScript">
+ <!--
+  this.focus();
+ //-->
+ </script>
 </head>
 <body topmargin="0" leftmargin="0" marginheight="0" marginwidth="0">
 
@@ -29,15 +31,7 @@
    $q       = "SELECT user_id FROM schedule_attendees WHERE schedule_id=$id";
    $attInfo = $g_db->getRows($q);
 
-   if ($mode == 1)
-   {
-    include "layers.inc.php";
-    include "scheduler/edit_scheduler_item.inc";
-   }
-   else
-   {
-    include "scheduler/view_scheduler_item.inc";
-   }
+   include "scheduler/view_scheduler_item.inc";
 ?>
 </body>
 </html>
