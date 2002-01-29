2d1eea744cfa2d0845de9266e81ceac42be74a49
Ivo Jansch (SVN Import) <info@achievo.org>
2002-01-29 23:23:19 +0000

    Added PIM screen, some fixes to scheduler and todo's.

--- index.php
+++ index.php
@@ -8,72 +8,18 @@
   $g_layout->output('<html>');
   $g_layout->head(text("app_title"));
   
-  if(strtolower($config_menu_pos) == "top")
-  {
-    if($config_top_frame==1)
-    {
-       $g_layout->output('
+
+  $g_layout->output('
         <frameset rows="70,*" frameborder="0" border="0">
           <frame name="top" scrolling="no" noresize src="top.php" marginwidth="0" marginheight="0">
        ');    
-    }
-    $g_layout->output('
-               <frameset rows="100,*" frameborder="0" border="0">
-                  <frame name="menu" scrolling="no"   noresize src="menu.php"   marginwidth="0" marginheight="0">
-                  <frame name="main" scrolling="auto" noresize src="'.session_url("dispatch.php?atknodetype=hours&atkaction=admin",SESSION_NEW).'" marginwidth="0" marginheight="0">
-    ');
-    if($config_top_frame==1) { $g_layout->output('</frameset>'); }
-    $g_layout->output('
-                   <noframes>
-                    <body bgcolor="#CCCCCC" text="#000000">
-                      <p>Your browser doesnt support frames, but this is required to run <?php echo $txt_app_title; ?></p>
-                    </body>
-                  </noframes>
-               </frameset>
-               </html>
-                   ');
-  }
-  if(strtolower($config_menu_pos) == "bottom")
-  {
-    if($config_top_frame==1)
-    {
-       $g_layout->output('
-        <frameset rows="70,*" frameborder="0" border="0">
-          <frame name="top" scrolling="no" noresize src="top.php" marginwidth="0" marginheight="0">
-       ');    
-    }
-    $g_layout->output('
-               <frameset rows="*,100" frameborder="0" border="0">
-                  <frame name="main" scrolling="auto" noresize src="'.session_url("dispatch.php?atknodetype=hours&atkaction=admin",SESSION_NEW).'" marginwidth="0" marginheight="0">
-                  <frame name="menu" scrolling="no"   noresize src="menu.php"   marginwidth="0" marginheight="0">
-    ');
-    if($config_top_frame==1) { $g_layout->output('</frameset>'); }
     
-    $g_layout->output('
-                   <noframes>
-                    <body bgcolor="#CCCCCC" text="#000000">
-                      <p>Your browser doesnt support frames, but this is required to run <?php echo $txt_app_title; ?></p>
-                    </body>
-                  </noframes>
-               </frameset>
-               </html>
-                   ');
-  }
-  elseif(strtolower($config_menu_pos) == "left")
-  {
-    if($config_top_frame==1)
-    {
-       $g_layout->output('
-        <frameset rows="70,*" frameborder="0" border="0">
-          <frame name="top" scrolling="no" noresize src="top.php" marginwidth="0" marginheight="0">
-       ');    
-    }
     $g_layout->output('
       <frameset cols="190,*" frameborder="0" border="0">
         <frame name="menu" scrolling="no" noresize src="menu.php" marginwidth="0" marginheight="0">
-        <frame name="main" scrolling="auto" noresize src="'.session_url("dispatch.php?atknodetype=hours&atkaction=admin",SESSION_NEW).'" marginwidth="0" marginheight="0">
+        <frame name="main" scrolling="auto" noresize src="'.session_url("dispatch.php?atknodetype=pim&atkaction=pim",SESSION_NEW).'" marginwidth="0" marginheight="0">
     ');
-    if($config_top_frame==1) { $g_layout->output('</frameset>'); }
+    $g_layout->output('</frameset>');
 
     $g_layout->output('
         <noframes>
@@ -83,34 +29,7 @@
         </noframes>
       </frameset>
       </html>
-       ');
-  }
-  elseif(strtolower($config_menu_pos)=="right")
-  {
-    if($config_top_frame==1)
-    {
-       $g_layout->output('
-        <frameset rows="70,*" frameborder="0" border="0">
-          <frame name="top" scrolling="no" noresize src="top.php" marginwidth="0" marginheight="0">
-       ');    
-    }
-    $g_layout->output('
-      <frameset cols="*,190" frameborder="0" border="0">
-        <frame name="main" scrolling="auto" noresize src="'.session_url("dispatch.php?atknodetype=hours&atkaction=admin",SESSION_NEW).'" marginwidth="0" marginheight="0">
-        <frame name="menu" scrolling="no" noresize src="menu.php" marginwidth="0" marginheight="0">
-    ');
-    if($config_top_frame==1) { $g_layout->output('</frameset>'); }
-
-    $g_layout->output('
-        <noframes>
-          <body bgcolor="#CCCCCC" text="#000000">
-            <p>Your browser doesnt support frames, but this is required to run <?php echo $txt_app_title; ?></p>
-          </body>
-        </noframes>
-      </frameset>
-      </html>
-       ');
-  }
+       ');  
 
   $g_layout->outputFlush();
 ?>
