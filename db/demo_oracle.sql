681cd4e4b44ed27e1e25f3196b0045a2ea46bf8f
Ivo Jansch (SVN Import) <info@achievo.org>
2002-03-12 20:53:49 +0000

    Readdition of all 0.7.3 unstable features that had been left out of 0.8. (Can be readded now, since 0.8 is now a separate cvs branch)

---
+++ db/demo_oracle.sql
@@ -0,0 +1,233 @@
+ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD';
+INSERT INTO accessright (node, action, entity) VALUES ( 'activity', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'activity', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'activity', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'activity', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'activity', 'stats', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contact', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contact', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contact', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contracts', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contracts', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contracts', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contracts', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contracttype', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contracttype', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contracttype', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'contracttype', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'costregistration', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'costregistration', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'costregistration', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'costregistration', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'customer', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'customer', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'customer', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'customer', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'employee', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'employee', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'employee', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'employee', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'employee', 'stats', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'employee', 'view_all', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.billing_project', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.billing_project', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.billing_project', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.billing_project', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.currency', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.currency', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.currency', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.currency', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.finance_customer', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.finance_customer', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.finance_customer', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.finance_customer', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.finance_project', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.finance_project', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.finance_project', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.finance_project', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.rates', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.rates', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.rates', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'finance.rates', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours', 'hoursurvey', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours', 'lock', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours', 'unlock', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours', 'view_all', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours_lock', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours_lock', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'hours_lock', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'phase', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'phase', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'phase', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'profile', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'profile', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'profile', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'profile', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project', 'planning', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project', 'stats', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project_notes', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project_notes', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project_notes', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'project_notes', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'schedule', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'schedule', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'schedule', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'schedule', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'schedule_types', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'schedule_types', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'schedule_types', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'schedule_types', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'todo', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'todo', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'todo', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'todo', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'tpl_phase', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'tpl_phase', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'tpl_phase', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'tpl_phase', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'tpl_project', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'tpl_project', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'tpl_project', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'tpl_project', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'usercontracts', 'add', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'usercontracts', 'admin', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'usercontracts', 'delete', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'usercontracts', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'userprefs', 'edit', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'weekreport', 'report', '1');
+INSERT INTO accessright (node, action, entity) VALUES ( 'weekreport', 'view_all', '1');
+
+INSERT INTO activity (id, name, description, remarkrequired) VALUES ( '1', 'Meeting', '', '0');
+INSERT INTO activity (id, name, description, remarkrequired) VALUES ( '2', 'Implementation', '', '0');
+INSERT INTO activity (id, name, description, remarkrequired) VALUES ( '3', 'Test', '', '0');
+INSERT INTO activity (id, name, description, remarkrequired) VALUES ( '4', 'Other', '', '1');
+INSERT INTO activity (id, name, description, remarkrequired) VALUES ( '5', 'Design/Specification', '', '0');
+
+INSERT INTO contact (id, lastname, firstname, address, zipcode, city, country, phone, cellular, fax, email, remark, company, owner) VALUES ( '1', 'Jansch', 'Ivo', '', '', '', '', '', '', '', 'ivo@ibuildings.nl', '', '1', NULL);
+
+INSERT INTO contract (id, billing_period, period_price, contracttype, customer, startdate, enddate, description) VALUES ( '1', 'Jaar', '150.00000', '2', '1', '2001-09-30', '2002-09-30', 'Support for Achievo.');
+
+INSERT INTO contracttype (id, description) VALUES ( '1', 'Hosting Contract');
+INSERT INTO contracttype (id, description) VALUES ( '2', 'Support Contract');
+
+INSERT INTO costregistration (id, userid, costdate, projectid, value, description, paymethod, currency) VALUES ( '1', 'ivo', '2001-10-01', '2', '20.00000', 'Bought a book about billing', 'cash', '0');
+
+INSERT INTO currency (name, value, symbol) VALUES ( 'Nederlandse Gulden', '2.20371', 'NLG');
+
+INSERT INTO customer (id, name, address, zipcode, city, country, phone, fax, email, website, bankaccount, remark, currency) VALUES ( '1', 'Ibuildings Zeeland BV', 'Gildeweg 39 A', '4381 PM', 'Vlissingen', 'Netherlands', '', '', 'info@ibuildings.nl', 'http://www.ibuildings.nl', '', '', '');
+
+INSERT INTO dependency (phaseid_row, phaseid_col) VALUES ( '2', '3');
+
+INSERT INTO employee (userid, name, email, password, status, theme, entity) VALUES ( 'ivo', 'Ivo Jansch', '', '098f6bcd4621d373cade4e832627b4f6', 'active', NULL, '1');
+
+INSERT INTO hours (id, entrydate, activitydate, phaseid, activityid, remark, time, userid) VALUES ( '1', '2001-10-01', '2001-10-01', '1', '4', 'Preparing 0.7.3 release', '240', 'ivo');
+
+INSERT INTO hours_lock (week, userid) VALUES ( '200133', '*');
+
+INSERT INTO phase (id, name, projectid, status, description, max_phasetime, max_hours) VALUES ( '1', 'Main phase', '1', 'active', '', '20', '20');
+INSERT INTO phase (id, name, projectid, status, description, max_phasetime, max_hours) VALUES ( '2', 'Specification/Design', '2', 'active', '', '0', '0');
+INSERT INTO phase (id, name, projectid, status, description, max_phasetime, max_hours) VALUES ( '3', 'Realisation', '2', 'active', '', '0', '0');
+INSERT INTO phase (id, name, projectid, status, description, max_phasetime, max_hours) VALUES ( '4', 'Support', '2', 'active', '', '0', '0');
+
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '1', '1', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '1', '2', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '1', '3', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '1', '4', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '2', '1', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '2', '4', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '2', '5', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '3', '1', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '3', '2', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '3', '3', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '3', '4', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '4', '1', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '4', '2', '0');
+INSERT INTO phase_activity (phaseid, activityid, billable) VALUES ( '4', '4', '0');
+
+INSERT INTO profile (id, name) VALUES ( '1', 'Administrators');
+
+INSERT INTO project (id, name, coordinator, status, description, startdate, customer, fixed_price) VALUES ( '1', 'Achievo 0.7', 'ivo', 'active', '', '20010930', '1', '0.00000');
+INSERT INTO project (id, name, coordinator, status, description, startdate, customer, fixed_price) VALUES ( '2', 'Achievo 1.0', 'ivo', 'active', '', '20010930', '1', '0.00000');
+
+INSERT INTO project_notes (id, projectid, owner, entrydate, description, title) VALUES ( '1', '2', 'ivo', '2001-09-30', 'Billing feature is not complete yet.', 'Billing');
+
+INSERT INTO rate (id, userid, projectid, activityid, customerid, startdate, enddate, description, rate, priority) VALUES ( '1', '0', '0', '0', '0', '2001-09-30', '2002-09-30', 'Standard hourly rate', '150.00000', '10');
+
+INSERT INTO schedule (id, startdate, enddate, starttime, endtime, title, description, location, allday, publicitem, owner, showowner, scheduletype) VALUES ( '1', '2001-10-01', '2001-10-01', '11:00:00', '13:00:00', 'Meeting with Achievo development team', 'Meeting about specs of billing feature.', 'Main meeting room', '0', '1', 'ivo', '0', '1');
+INSERT INTO schedule (id, startdate, enddate, starttime, endtime, title, description, location, allday, publicitem, owner, showowner, scheduletype) VALUES ( '2', '2001-10-01', '2001-10-01', '14:30:00', '15:30:00', 'Dentist', 'Go see the dentist.', 'Dentist office', '0', '1', 'ivo', '0', '2');
+
+INSERT INTO schedule_types (id, description, bgcolor, fgcolor) VALUES ( '1', 'Meeting', '#0055EE', '#FFFFFF');
+INSERT INTO schedule_types (id, description, bgcolor, fgcolor) VALUES ( '2', 'Default', '#FFFF00', '#000044');
+
+INSERT INTO todo (id, projectid, owner, assigned_to, entrydate, duedate, description, title, status, priority, mailnotification) VALUES ( '1', '2', 'ivo', 'ivo', '2001-09-30', '2001-09-30', 'All current features must at least be stable before 1.0 release.', 'Make everything stable', '1', '2', '0');
+
+INSERT INTO tpl_dependency (phaseid_row, phaseid_col, projectid) VALUES ( '1', '2', '1');
+
+INSERT INTO tpl_phase (id, name, description) VALUES ( '1', 'Specification/Design', '');
+INSERT INTO tpl_phase (id, name, description) VALUES ( '2', 'Realisation', '');
+INSERT INTO tpl_phase (id, name, description) VALUES ( '3', 'Support', '');
+
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '1', '1');
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '1', '4');
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '1', '5');
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '2', '1');
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '2', '2');
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '2', '3');
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '2', '4');
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '3', '1');
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '3', '2');
+INSERT INTO tpl_phase_activity (phaseid, activityid) VALUES ( '3', '4');
+
+INSERT INTO tpl_project (id, name, description) VALUES ( '1', 'Standard 3 Phase project', 'Template for all projects that consist of 3 phases.');
+
+INSERT INTO tpl_project_phase (projectid, phaseid) VALUES ( '1', '1');
+INSERT INTO tpl_project_phase (projectid, phaseid) VALUES ( '1', '2');
+INSERT INTO tpl_project_phase (projectid, phaseid) VALUES ( '1', '3');
+
+INSERT INTO usercontract (id, description, startdate, enddate, userid, uc_hours) VALUES ( '1', 'Standard full-time contract', '2001-10-01', '2002-10-01', 'ivo', '40');
+
+DROP SEQUENCE seq_project;
+DROP SEQUENCE seq_profile;
+DROP SEQUENCE seq_activity;
+DROP SEQUENCE seq_tpl_phase;
+DROP SEQUENCE seq_tpl_project;
+DROP SEQUENCE seq_contracttype;
+DROP SEQUENCE seq_schedule_types;
+DROP SEQUENCE seq_customer;
+DROP SEQUENCE seq_contract;
+DROP SEQUENCE seq_contact;
+DROP SEQUENCE seq_phase;
+DROP SEQUENCE seq_todo;
+DROP SEQUENCE seq_project_notes;
+DROP SEQUENCE seq_rate;
+DROP SEQUENCE seq_schedule;
+DROP SEQUENCE seq_costregistration;
+DROP SEQUENCE seq_hours;
+DROP SEQUENCE seq_usercontract;
+
+CREATE SEQUENCE seq_project INCREMENT BY 1 START WITH 3 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_profile INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_activity INCREMENT BY 1 START WITH 6 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_tpl_phase INCREMENT BY 1 START WITH 4 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_tpl_project INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_contracttype INCREMENT BY 1 START WITH 3 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_schedule_types INCREMENT BY 1 START WITH 3 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_customer INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_contract INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_contact INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_phase INCREMENT BY 1 START WITH 5 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_todo INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_project_notes INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_rate INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_schedule INCREMENT BY 1 START WITH 3 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_costregistration INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_hours INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
+CREATE SEQUENCE seq_usercontract INCREMENT BY 1 START WITH 2 NOCYCLE NOORDER;
---
+++ upgrade/convert-0.8-to-0.9.sql
@@ -0,0 +1,74 @@
+CREATE TABLE bill (
+   id int(10) unsigned DEFAULT '0' NOT NULL,
+   projectid int(10) DEFAULT '0' NOT NULL,
+   description text,
+   create_date date DEFAULT '0000-00-00' NOT NULL,
+   userid varchar(15) NOT NULL,
+   status varchar(30) NOT NULL,
+   specify_hours tinyint(1) DEFAULT '0' NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE rate (
+   id int(10) unsigned DEFAULT '0' NOT NULL,
+   userid varchar(15),
+   projectid int(10),
+   activityid int(10),
+   customerid int(10),
+   startdate date DEFAULT '0000-00-00' NOT NULL,
+   enddate date DEFAULT '0000-00-00' NOT NULL,
+   description text,
+   rate decimal(13,5) DEFAULT '0.00000' NOT NULL,
+   priority tinyint(4) DEFAULT '0' NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE costregistration (
+   id int(10) unsigned DEFAULT '0' NOT NULL,
+   userid varchar(15) NOT NULL,
+   costdate date DEFAULT '0000-00-00' NOT NULL,
+   projectid int(10) unsigned DEFAULT '0' NOT NULL,
+   value decimal(13,5) DEFAULT '0.00000' NOT NULL,
+   description text,
+   paymethod varchar(30) NOT NULL,
+   currency varchar(30),
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE currency (
+   name varchar(30) DEFAULT '0' NOT NULL,
+   value decimal(13,5) DEFAULT '0.00000' NOT NULL,
+   symbol varchar(10) NOT NULL,
+   PRIMARY KEY (symbol)
+);
+
+CREATE TABLE schedule (
+   id int(10) unsigned DEFAULT '0' NOT NULL,
+   startdate date DEFAULT '0000-00-00' NOT NULL,
+   enddate date DEFAULT '0000-00-00' NOT NULL,
+   starttime time DEFAULT '00:00:00' NOT NULL,
+   endtime time DEFAULT '00:00:00' NOT NULL,
+   title varchar(50) NOT NULL,
+   description text,
+   location varchar(50) DEFAULT '0',
+   allday tinyint(1) DEFAULT '0' NOT NULL,
+   publicitem tinyint(1) DEFAULT '0' NOT NULL,
+   owner varchar(15) NOT NULL,
+   showowner tinyint(1) DEFAULT '0' NOT NULL,
+   scheduletype int(10) DEFAULT '0' NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE schedule_attendees (
+   scheduleid int(10) DEFAULT '0' NOT NULL,
+   userid varchar(15) NOT NULL,
+   PRIMARY KEY (scheduleid, userid)
+);
+
+CREATE TABLE schedule_types (
+   id int(10) unsigned DEFAULT '0' NOT NULL,
+   description varchar(50) NOT NULL,
+   bgcolor varchar(7) NOT NULL,
+   fgcolor varchar(7) NOT NULL,
+   PRIMARY KEY (id)
+);
