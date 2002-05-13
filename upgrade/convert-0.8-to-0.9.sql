49068a36cc40fcc438e510f48951167f7f7846a1
Ludo (SVN Import) <info@achievo.org>
2002-05-13 09:01:09 +0000

    Removed obsolete '*' before "admin" action for the project class (config.nodes.inc). Updated MySQL upgrade script accordingly.

--- upgrade/convert-0.8-to-0.9.sql
+++ upgrade/convert-0.8-to-0.9.sql
@@ -99,5 +99,7 @@ CREATE TABLE schedule_types (
    PRIMARY KEY (id)
 );
 
- ALTER TABLE project ADD abbreviation VARCHAR(10) NOT NULL;
- ALTER TABLE accessright CHANGE node node VARCHAR(50) NOT NULL;
+ALTER TABLE project ADD abbreviation VARCHAR(10) NOT NULL;
+ALTER TABLE accessright CHANGE node node VARCHAR(50) NOT NULL;
+
+UPDATE accessright SET action = 'admin' WHERE node = 'project' AND action = '*admin';
