01fbb90b279dbc1fc6d61af981d00fbf5ca5d655
Ludo (SVN Import) <info@achievo.org>
2002-05-13 09:27:00 +0000

    The update for the MySQL upgrade script revision 1.4 seems to be unnecessary because the * was already filtered before entering the data in to the accesright table, so that fix has been removed

--- upgrade/convert-0.8-to-0.9.sql
+++ upgrade/convert-0.8-to-0.9.sql
@@ -101,5 +101,3 @@ CREATE TABLE schedule_types (
 
 ALTER TABLE project ADD abbreviation VARCHAR(10) NOT NULL;
 ALTER TABLE accessright CHANGE node node VARCHAR(50) NOT NULL;
-
-UPDATE accessright SET action = 'admin' WHERE node = 'project' AND action = '*admin';
