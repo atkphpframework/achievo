5fb267f5f149057d962665d68820e9615d689fcd
Ivo Jansch (SVN Import) <info@achievo.org>
2002-05-23 22:59:13 +0000

    Forgot to rename one date in sql file.

--- modules/resource_planning/db/mysql.sql
+++ modules/resource_planning/db/mysql.sql
@@ -2,7 +2,7 @@ CREATE TABLE planning (
   id int(10) NOT NULL default '0',
   phaseid int(10) NOT NULL default '0',
   employeeid varchar(15) NOT NULL default '0',
-  date date NOT NULL default '0000-00-00',
+  plandate date NOT NULL default '0000-00-00',
   time varchar(20) NOT NULL default '',
   description varchar(250) NOT NULL default '',
   PRIMARY KEY  (id)
