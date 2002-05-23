16d95a002557cdc04cd904199a5b9439a31df795
Ivo Jansch (SVN Import) <info@achievo.org>
2002-05-23 23:01:01 +0000

    Added todo_history table and renamed date to plandate.

--- upgrade/convert-0.9-to-0.9.1.sql
+++ upgrade/convert-0.9-to-0.9.1.sql
@@ -1,11 +1,11 @@
-ALTER TABLE project ADD abbreviation VARCHAR(10) NOT NULL;
-ALTER TABLE accessright CHANGE node node VARCHAR(50) NOT NULL;
+ALTER TABLE project ADD abbreviation varchar(10) NOT NULL;
+ALTER TABLE accessright CHANGE node node varchar(50) NOT NULL;
 
 CREATE TABLE planning (
   id int(10) NOT NULL default '0',
   phaseid int(10) NOT NULL default '0',
   employeeid varchar(15) NOT NULL default '0',
-  date date NOT NULL default '0000-00-00',
+  plandate date NOT NULL default '0000-00-00',
   time varchar(20) NOT NULL default '',
   description varchar(250) NOT NULL default '',
   PRIMARY KEY  (id)
@@ -16,3 +16,21 @@ CREATE TABLE employee_project (
   projectid int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (employeeid,projectid)
 );
+
+CREATE TABLE todo_history (
+   id int(10) unsigned DEFAULT '0' NOT NULL,
+   todoid int(10) unsigned DEFAULT '0' NOT NULL,
+   updated datetime DEFAULT '0000-00-00 00:00' NOT NULL,
+   projectid int(10) unsigned DEFAULT '0' NOT NULL,
+   owner varchar(15) NOT NULL,
+   assigned_to varchar(100) NOT NULL,
+   entrydate date DEFAULT '0000-00-00' NOT NULL,
+   duedate date DEFAULT '0000-00-00' NOT NULL,
+   description text,
+   title varchar(50) NOT NULL,
+   status tinyint(4) DEFAULT '0' NOT NULL,
+   priority tinyint(4) DEFAULT '0' NOT NULL,
+   PRIMARY KEY (id)
+);
+
+ALTER TABLE todo ADD updated datetime DEFAULT '0000-00-00 00:00' NOT NULL;
