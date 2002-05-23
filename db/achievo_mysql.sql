0ad8d26942193e892fb6366c52c3f28cae445c8b
Ivo Jansch (SVN Import) <info@achievo.org>
2002-05-23 19:57:43 +0000

    Added resource planning tables.

--- db/achievo_mysql.sql
+++ db/achievo_mysql.sql
@@ -309,6 +309,22 @@ CREATE TABLE usercontract (
    PRIMARY KEY (id)
 );
 
+CREATE TABLE planning (
+  id int(10) NOT NULL default '0',
+  phaseid int(10) NOT NULL default '0',
+  employeeid varchar(15) NOT NULL default '0',
+  date date NOT NULL default '0000-00-00',
+  time varchar(20) NOT NULL default '',
+  description varchar(250) NOT NULL default '',
+  PRIMARY KEY  (id)
+);
+
+CREATE TABLE employee_project (
+  employeeid varchar(10) NOT NULL default '0',
+  projectid int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (employeeid,projectid)
+);
+
 CREATE TABLE db_sequence (
    seq_name varchar(20) NOT NULL,
    nextid int(10) unsigned DEFAULT '0' NOT NULL,
