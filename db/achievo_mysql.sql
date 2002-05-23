29e691adbac299023c711ba65cdb4668b9b778d2
Ivo Jansch (SVN Import) <info@achievo.org>
2002-05-23 22:51:32 +0000

    Added the todo history tables. Renamed the 'date' column of planning to 'plandate' (date is a reserved word and won't work on oracle)

--- db/achievo_mysql.sql
+++ db/achievo_mysql.sql
@@ -287,6 +287,7 @@ CREATE TABLE schedule_types (
 
 CREATE TABLE todo (
    id int(10) unsigned DEFAULT '0' NOT NULL,
+   updated datetime DEFAULT '0000-00-00 00:00' NOT NULL,
    projectid int(10) unsigned DEFAULT '0' NOT NULL,
    owner varchar(15) NOT NULL,
    assigned_to varchar(100) NOT NULL,
@@ -309,11 +310,17 @@ CREATE TABLE usercontract (
    PRIMARY KEY (id)
 );
 
+CREATE TABLE db_sequence (
+   seq_name varchar(20) NOT NULL,
+   nextid int(10) unsigned DEFAULT '0' NOT NULL,
+   PRIMARY KEY (seq_name)
+);
+
 CREATE TABLE planning (
   id int(10) NOT NULL default '0',
   phaseid int(10) NOT NULL default '0',
   employeeid varchar(15) NOT NULL default '0',
-  date date NOT NULL default '0000-00-00',
+  plandate date NOT NULL default '0000-00-00',
   time varchar(20) NOT NULL default '',
   description varchar(250) NOT NULL default '',
   PRIMARY KEY  (id)
@@ -325,8 +332,18 @@ CREATE TABLE employee_project (
   PRIMARY KEY  (employeeid,projectid)
 );
 
-CREATE TABLE db_sequence (
-   seq_name varchar(20) NOT NULL,
-   nextid int(10) unsigned DEFAULT '0' NOT NULL,
-   PRIMARY KEY (seq_name)
+CREATE TABLE todo_history (
+   id int(10) unsigned DEFAULT '0' NOT NULL,   
+   todoid int(10) unsigned DEFAULT '0' NOT NULL,
+   updated datetime DEFAULT '0000-00-00 00:00' NOT NULL,
+   projectid int(10) unsigned DEFAULT '0' NOT NULL,
+   owner varchar(15) NOT NULL,
+   assigned_to varchar(100) NOT NULL,
+   entrydate date DEFAULT '0000-00-00' NOT NULL,
+   duedate date DEFAULT '0000-00-00' NOT NULL,
+   description text,
+   title varchar(50) NOT NULL,
+   status tinyint(4) DEFAULT '0' NOT NULL,
+   priority tinyint(4) DEFAULT '0' NOT NULL,
+   PRIMARY KEY (id)
 );
