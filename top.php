afdf2e9772ff3d3883b780f3b150c53a8003851a
Ivo Jansch (SVN Import) <info@achievo.org>
2002-06-04 18:47:31 +0000

    Added searchfacility to topframe (search is yet to be implemented though).

--- top.php
+++ top.php
@@ -9,16 +9,35 @@
   $g_layout->body();
 
   $g_layout->ui_top($txt_app_title);
-
-  $g_layout->output('<br>'.$txt_logout_loggedinuser.': &nbsp;<b>'.$g_user["name"].'</b>&nbsp; &nbsp; &nbsp;');  
-  $g_layout->output(href(dispatch_url("pim", "pim"), text('pim'), SESSION_NEW, false, 'target="main"').'&nbsp; &nbsp; &nbsp;');
+  
+  $table = $g_layout->ret_table_simple(0,true);
+  
+  $table.= '<tr>';
+  
+  $table.= $g_layout->ret_td("&nbsp;", 'width="25%" height="40"');
+  
+  $centerpiece = $txt_logout_loggedinuser.': &nbsp;<b>'.$g_user["name"].'</b>&nbsp; &nbsp; &nbsp;';  
+  $centerpiece.= href(dispatch_url("pim", "pim"), text('pim'), SESSION_NEW, false, 'target="main"').'&nbsp; &nbsp; &nbsp;';
+  
   if ($g_user["name"]!="administrator")
   {
-    $g_layout->output(href(dispatch_url("userprefs", "edit", array("atkselector" => "employee.userid='".$g_user["name"]."'")), text('userprefs'), SESSION_NEW, false, 'target="main"').'&nbsp; &nbsp; &nbsp;');
+    $centerpiece.= href(dispatch_url("userprefs", "edit", array("atkselector" => "employee.userid='".$g_user["name"]."'")), text('userprefs'), SESSION_NEW, false, 'target="main"').'&nbsp; &nbsp; &nbsp;';
   }
   
-  $g_layout->output('<a href="index.php?atklogout=1" target="_top">'.text('logout').'</a>');
-  $g_layout->output('<br><br>');
+  $centerpiece.= '<a href="index.php?atklogout=1" target="_top">'.text('logout').'</a>';
+  
+  $table.= $g_layout->ret_td($centerpiece, 'width="50%" align="center"');
+  
+  $searchpiece = '<form action="search.php" target="main">';
+  $searchpiece.= session_form(SESSION_NEW);
+  $searchpiece.= '<input name="searchstring" type="text" size="20">&nbsp;<input type="submit" value="'.text("search").'">';
+  $searchpiece.= '</form>';
+  
+  $table.= $g_layout->ret_td($searchpiece, 'width="25%" align="right"');    
+      
+  $table .= '</tr></table>';
+    
+  $g_layout->output($table);
   $g_layout->ui_bottom();
   $g_layout->output('</body>');
   $g_layout->output('</html>');
