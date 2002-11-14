7899b4b04aa1ca4fd80a00dff4f5913baba70ccd
Lineke (SVN Import) <info@achievo.org>
2002-11-14 08:49:59 +0000

    Link to userprefs used employee table, changed to person table

--- top.php
+++ top.php
@@ -1,6 +1,6 @@
 <?php
-  include_once("atk.inc");  
-  atksession();  
+  include_once("atk.inc");
+  atksession();
   atksecure();
   include_once("./theme.inc");
 
@@ -9,36 +9,36 @@
   $g_layout->body();
 
   $g_layout->ui_top($txt_app_title);
-  
+
   $table = $g_layout->ret_table_simple(0,true);
-  
+
   $table.= '<tr>';
-  
+
   $table.= $g_layout->ret_td("&nbsp;", 'width="20%" height="40"');
-  
-  $centerpiece = $txt_logout_loggedinuser.': &nbsp;<b>'.$g_user["name"].'</b>&nbsp; &nbsp; &nbsp;';  
+
+  $centerpiece = $txt_logout_loggedinuser.': &nbsp;<b>'.$g_user["name"].'</b>&nbsp; &nbsp; &nbsp;';
   $centerpiece.= href(dispatch_url("pim.pim", "pim"), text('pim'), SESSION_NEW, false, 'target="main"').'&nbsp; &nbsp; &nbsp;';
-  
+
   if ($g_user["name"]!="administrator")
   {
-    $centerpiece.= href(dispatch_url("employee.userprefs", "edit", array("atkselector" => "employee.userid='".$g_user["name"]."'")), text('userprefs'), SESSION_NEW, false, 'target="main"').'&nbsp; &nbsp; &nbsp;';
+    $centerpiece.= href(dispatch_url("employee.userprefs", "edit", array("atkselector" => "person.userid='".$g_user["name"]."'")), text('userprefs'), SESSION_NEW, false, 'target="main"').'&nbsp; &nbsp; &nbsp;';
   }
-  
+
   $centerpiece.= '<a href="index.php?atklogout=1" target="_top">'.text('logout').'</a>';
-  
+
   $table.= $g_layout->ret_td($centerpiece, 'width="55%" align="center"');
-  
+
   $searchnode = getNode("search.search");
   $searchpiece = $searchnode->simpleSearchForm("", "main", SESSION_NEW);
-  
-  $table.= $g_layout->ret_td($searchpiece, 'width="25%" align="right"');    
-      
+
+  $table.= $g_layout->ret_td($searchpiece, 'width="25%" align="right"');
+
   $table .= '</tr></table>';
-    
+
   $g_layout->output($table);
   $g_layout->ui_bottom();
   $g_layout->output('</body>');
   $g_layout->output('</html>');
-  
+
   $g_layout->outputFlush();
 ?>
