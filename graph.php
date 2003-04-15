dccfc4e0b50e5196c1f56a4937b0338a69afbff4
Ivo Jansch (SVN Import) <info@achievo.org>
2003-04-15 21:03:39 +0000

    * Graph.php wrapper now makes use of plotter companion classes so each   graphattribute can have it's own plot method.

--- graph.php
+++ graph.php
@@ -1,5 +1,33 @@
 <?php   
 
+  /**
+   * graph.php
+   *
+   * This is a wrapper file which produces images for the graph module.
+   * You should not need to use this file directly from your code. See the
+   * documentation of the graphAttribute and/or dataGraphAttribute attributes
+   * in the graph module for instructions how to use the graph module.
+   *
+   * @author Ivo Jansch <ivo@achievo.org>
+   *
+   * @version $Revision$
+   *
+   * $Id$
+   *
+   * This file is part of Achievo.
+   *
+   * Achievo is free software; you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation; either version 2 of the License, or
+   * (at your option) any later version.
+   *
+   * Achievo is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+   *
+   */
+
   /* Setup the system */    
   include_once("atk.inc");    
 
@@ -9,30 +37,29 @@
     
   atksession("img", false);
   atksecure();
-      
-  // Create node
-  $obj = &getNode($ATK_VARS["atknodetype"]); 
-    
-  if (is_object($obj))
+  
+  // Create attribute. 
+  $plottersource = $ATK_VARS["plotter"];
+  useattrib($plottersource);
+  list($module, $attribname) = explode(".", $plottersource);
+  
+  $plotterclass = $attribname."Plotter";
+
+  $res = false;
+  
+  if (class_exists($plotterclass))
   {
-    // We prepend graph_ to the callback as a security precaution. 
-    // This prevents anyone from executing arbitrary methods. 
-    // Only methods starting with graph_ can be executed as callback.
-    // 
-    $method = "graph_".$ATK_VARS["callback"];
-    if (method_exists($obj, $method))
-    {
-      $res = $obj->$method($ATK_VARS);     
-    }
-    else
-    {
-      atkerror("Graph: callback $method on source node ".$ATK_VARS["atknodetype"]." does not exist.");
-    }
+    // Since php does not support calling static methods
+    // on a 'dynamic' class (where the name is inside a
+    // variable), we use a companion plotter class to 
+    // plot stuff.
+    $plotter = &new $plotterclass();
+    $res = $plotter->plot($ATK_VARS);
   }
   else
-  {
-    atkerror("Graph: source node ".$ATK_VARS["atknodetype"]." not found.");
-  }
+  {  
+    atkerror("Graph: plotter ".$plotterclass." not found.");
+  }  
   
   if (!$res)
   {
