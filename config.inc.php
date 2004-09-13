9f70d8539d17ddbd90614db3fec9fad3c1406461
Ivo Jansch (SVN Import) <info@achievo.org>
2004-09-13 22:57:34 +0000

    Reset defaults that somehow were changed in a previous  merge.

--- config.inc.php
+++ config.inc.php
@@ -15,16 +15,15 @@
   // MySQL is currently the only officially supported database.
   // Experimental: If you want to use PostgreSQL, change this value to 
   // "pgsql". For Oracle, use "oci8" or "oci9".
-  $config_database = "pgsql";
+  $config_database = "mysql";
 
   // The database configuration. Specify the hostname of the database server,
   // the database to use and the user/password.
   //
   $config_databasehost = "localhost";
-  $config_databasename = "achievo";  
-  $config_databaseuser = "achievo";
-  $config_databasepassword = "achievo";
-
+  $config_databasename = "achievo_1_1";  
+  $config_databaseuser = "demo";
+  $config_databasepassword = "demo";
 
   // -------------------------------------------
   //           LAYOUT CONFIGURATION
