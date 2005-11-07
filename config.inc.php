3276a6436c22ed5eaa11c93696c9d959a4c838be
Ivo Jansch (SVN Import) <info@achievo.org>
2005-11-07 11:58:49 +0000

    Reset administrator password. In CVS, it should be 'demo'

--- config.inc.php
+++ config.inc.php
@@ -77,7 +77,7 @@
   // set up the entire system and enable it only when you need it.
   // (disable it by putting // in front of it)
   //
-  $config_administratorpassword = "topsecret";
+  $config_administratorpassword = "demo";
 
   // If you set $config_auth_dropdown to true, the login screen will contain
   // a list of available usernames. If you set it to false, there will be
