d3940635b1c39c680ae0691e740a17562246ca38
Dennis Luitwieler (SVN Import) <info@achievo.org>
2006-01-12 11:29:16 +0000

    Added configurable initial status. [Ticket: 8056]

---
+++ configs/calendar.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+  // This variable configures what the default status of a scheduled item should be.   
+  // if omitted, the default status is 'unconfirmed'.
+  $config["calendar_schedule_defaultstatus"] = "confirmed";  
+?>
