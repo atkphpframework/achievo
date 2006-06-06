2d75292708f2aa35a1d44a4b2c38648f8f92b29d
Guido (SVN Import) <info@achievo.org>
2006-06-06 19:49:20 +0000

    * Removed duplicate </html> tag. [Guido] * Index.php now uses atkoutput instead of an evil(tm)(r)(c) echo() call   to send the frameset document to the browser. [Guido] * Fixed the login problem where you would see a login box in the menu and   top frame after logging in. This was caused by the menu and top pages   not being requested through a session_url. [Guido]

--- index.php
+++ index.php
@@ -9,79 +9,67 @@
   $page = &atknew("atk.ui.atkpage");
   $ui = &atkinstance("atk.ui.atkui");
   $theme = &atkTheme::getInstance();
-  //$output = &atkOutput::getInstance();
-  $output = "";
+  $atkoutput = &atkOutput::getInstance();
 
-  $output='<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">';
-  $output.="\n<html>\n <head>\n";
-  $output.='  <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset='.atktext("charset","","atk").'">';
-  $output.="\n  <title>".atktext('app_title')."</title>\n </head>\n";
 
   $user = getUser();
   $pda = browserInfo::detectPDA();
-  atkimport("atk.utils.atkframeset");
 
   if(isset($ATK_VARS["atknodetype"]) && isset($ATK_VARS["atkaction"]))
   {
-    $default_url = "dispatch.php?atknodetype=".$ATK_VARS["atknodetype"]."&atkaction=".$ATK_VARS["atkaction"];
-    if (isset($ATK_VARS["atkselector"])) $default_url.="&atkselector=".$ATK_VARS["atkselector"];
+    $params = array();
+    if (isset($ATK_VARS["atkselector"])) $params["atkselector"] = $ATK_VARS["atkselector"];
+    $default_url = session_url(dispatch_url($ATK_VARS["atknodetype"], $ATK_VARS["atkaction"], $params), SESSION_NEW);
   }
   else if (strtolower($user["name"]) == "administrator")
   {
-    $default_url = session_url("dispatch.php?atknodetype=pim.pim&atkaction=adminpim",SESSION_NEW);
-  }
-  else if ($pda)
-  {
-    $default_url = session_url("dispatch.php?atknodetype=pim.pim&atkaction=pim",SESSION_NEW);
+    $default_url = session_url(dispatch_url("pim.pim", "adminpim"),SESSION_NEW);
   }
   else
   {
-    $default_url = session_url("dispatch.php?atknodetype=pim.pim&atkaction=pim",SESSION_NEW);
+    $default_url = session_url(dispatch_url("pim.pim", "pim"),SESSION_NEW);
   }
+  $top_url = session_url("top.php", SESSION_NEW);
+  $menu_url = session_url("menu.php", SESSION_NEW);
 
-  //$root = &new atkRootFrameset();
+  $output ='<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">';
+  $output.="\n<html>\n  <head>\n";
+  $output.='    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset='.atktext("charset","","atk").'">';
+  $output.="\n    <title>".atktext('app_title')."</title>\n  </head>\n";
   if (!$pda)
   {
-  $output.='
-        <frameset rows="80,*" frameborder="0" border="0">
-          <frame name="top" scrolling="no" noresize src="top.php" marginwidth="0" marginheight="0">
-       ';
-
-  $output.='
-      <frameset cols="190,*" frameborder="0" border="0">
-        <frame name="menu" scrolling="no" noresize src="menu.php" marginwidth="0" marginheight="0">
-        <frame name="main" scrolling="auto" noresize src="'.$default_url.'" marginwidth="0" marginheight="0">
-    ';
-  $output.='</frameset>';
-
-  $output.='
-        <noframes>
-          <body bgcolor="#CCCCCC" text="#000000">
-            <p>Your browser doesnt support frames, but this is required to run '.atktext("app_title").'</p>
-          </body>
-        </noframes>
-      </frameset>
-      </html>
-       ';
+    $output.='  <frameset rows="80,*" frameborder="0" border="0">
+    <frame name="top" scrolling="no" noresize src="'.$top_url.'" marginwidth="0" marginheight="0">
+    <frameset cols="190,*" frameborder="0" border="0">
+      <frame name="menu" scrolling="no" noresize src="'.$menu_url.'" marginwidth="0" marginheight="0">
+      <frame name="main" scrolling="auto" noresize src="'.$default_url.'" marginwidth="0" marginheight="0">
+    </frameset>
+    <noframes>
+      <body bgcolor="#CCCCCC" text="#000000">
+        <p>Your browser doesnt support frames, but this is required to run '.atktext("app_title").'</p>
+      </body>
+    </noframes>
+  </frameset>
+';
   }
   else
   {
     $config_defaulttheme="default";
 
-    $output.='
-    <frameset rows="30,*" frameborder="0">
-      <frame name="top" src="top.php">
-      <frame name="main" src="'.$default_url.'">
-      <noframes>
-        <body bgcolor="#CCCCCC" text="#000000">
-          <p>Your PDA doesnt support frames, but this is required to run '.atktext("app_title").'</p>
-        </body>
-      </noframes>
-    </frameset>
-  ';
+    $output.='  <frameset rows="30,*" frameborder="0">
+    <frame name="top" src="'.$top_url.'">
+    <frame name="main" src="'.$default_url.'">
+    <noframes>
+      <body bgcolor="#CCCCCC" text="#000000">
+        <p>Your PDA doesnt support frames, but this is required to run '.atktext("app_title").'</p>
+      </body>
+    </noframes>
+  </frameset>
+';
   }
-
-
   $output.= "</html>";
-  echo $output;
+
+  // Send the output to the browser
+  $atkoutput->output($output);
+  $atkoutput->outputFlush();
 ?>
