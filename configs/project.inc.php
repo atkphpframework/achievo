9ae748b250a1948cac446e67f80c2f51d68298f8
Dennis Luitwieler (SVN Import) <info@achievo.org>
2006-09-01 12:08:07 +0000

    Moved the cascading delete from the module.inc to the class.phase.inc file (where it belongs). Also added some comment in the project config file.

--- configs/project.inc.php
+++ configs/project.inc.php
@@ -1,4 +1,6 @@
 <?php
 
+  // This variable configures wether hour registrations should be automatically deleted when
+  // a phase is deleted. If omitted, false is default.
   $config["project_cascading_delete_hours"] = false; 
 ?>
