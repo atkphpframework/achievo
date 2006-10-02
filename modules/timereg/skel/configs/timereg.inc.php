07cbb731825fbe719512f012bc2c68f700b325f6
Harrie Verveer (SVN Import) <info@achievo.org>
2006-10-02 08:09:02 +0000

    - Added possibility to approve or disapprove hours per coordinator - Added possibility to e-mail coordinators on hours blocking

---
+++ modules/timereg/skel/configs/timereg.inc.php
@@ -0,0 +1,22 @@
+<?
+
+  /**
+   * When the setting below is set to 'true', only employees
+   * that are projectmembers of the projects the current user
+   * is projectmanager of will be displayed.
+   */
+  $config["hours_approve_projectteam_members_only"] = false;
+  
+  /**
+   * When the setting below is set to 'true', the user will
+   * only see hours to approve that are not already approved.
+   */
+  $config["hours_approve_only_show_not_approved"] = false;
+  
+  /**
+   * When the setting below is set to 'true', the week that
+   * is disaproved will automatically get unlocked.
+   */
+  $config["hours_approve_unlock_on_disaprove"] = false;
+
+?>
