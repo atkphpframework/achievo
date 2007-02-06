1035c2db83bb3ca99cbc837a7051049288fb9f36
Ivo Jansch (SVN Import) <info@achievo.org>
2007-02-06 14:46:40 +0000

    The Achievo Tomorrow Branch Landing:
    + Added Ajax functionality in hours node. [Dennis]
    + atkManyToOneRelations that have the AF_LARGE flag set are autocompletion
      fields. [Dennis]
    + Added initial value for workperiod in hours node for autocompletion
      fields. [Dennis]
    + Added a phase-template grid to the project node. From this grid phase
      templates can be selected. [Dennis]
    + Time registration can now be locked per month. [Roger, June]
    * Fixed translation of the Reports > Projectstatus page title. [Guido]
    * Fixed descriptor of organization.contracts node [Dennis]
    * Fixed: projectstatus now also shows phases without a startdate
      and/or enddate. [Dennis]
    * Fixed: fatal error in class.project.inc. [Dennis]
    * Fixed: Removed border from atkboolattribute in Internet Explorer.
      [Dennis]
    * Fixed: javascript error when clicking on a column in the
      weekreport. [dennis]
    * Fixed: startdate and enddate were not correctly determined in
      the weekreport node. [Dennis]
    * Fixed: When using a graphAttribute on a page, the submit buttons did
      not work in IE due to a subform added by the graphAttribute.
      [Peter,Dennis]
    * Fixed: The stats tab in project node wasn't visible in the view action.
      [Peter, Dennis]
    * Fixed: When using the graph module, PHP errors and deprecated messages
      were visible to the user. Configured jpg-config.inc to hide these
      messages. [Peter, Dennis]
    * Fixed: Link-Name of phase on select-page of the phasenode was broken and
      caused a 'session expired' notice. [Dennis]
    * In the hours node, the viewuser dropdown can be configered by overriding
      the functions viewuserFilter and viewuserOrderBy and uses the descriptor
      template of the employee node. [Dennis]
    * The hoursnode that is used by the hoursurvey can be set to a different node
      by overriding the function getHoursNodeName(). [Dennis]
    * Basereport now also supports autocompletion. [Dennis, Guido, Peter]
    * Fixed: initial user was always reset in the hoursnode. [Dennis]
    * Social security and bankaccount number fields are now hidden when adding a
      new employee. [Ivo]
    * When adding a new project, the currently logged in user is now set as
      the project coordinator by default. [Ivo]
    * Moved achievo_modern theme to new location. This should go unnoticed but
      you may need to clear the achievotmp/themes dir when upgrading. [Ivo]

--- modules/timereg/skel/configs/timereg.inc.php
+++ modules/timereg/skel/configs/timereg.inc.php
@@ -1,5 +1,6 @@
-<?
-
+<?php
+  
+  
   /**
    * When the setting below is set to 'true', only employees
    * that are projectmembers of the projects the current user
@@ -18,6 +19,31 @@
    * is disaproved will automatically get unlocked.
    */
   $config["hours_approve_unlock_on_disaprove"] = false;
+
+  /**
+   * Decides if locking should be based on months or
+   * weeks. Valid entries are 'week' and 'month'
+  */
+  $config["lockmode"] = "week"; 
+  
+  /**
+   * When this setting is set to 'true', each coordinator
+   * approves hours for his/her projects.
+  */
+  $config["approve_period_per_coordinator"] = false;
+
+  /**
+   * Setting related to approve_period_per_coordinator.
+   * Will mail coordinators when hours are locked.
+   *
+  */
+  $config["mail_coordinators_on_lock"] = true;
+
+  /**
+   * The mail address from which the mails will be
+   * sent.
+  */
+  $config["from"] = "Dummy <dummy@dummy.com>";  
   
   /**
    * Set a date from when the hoursnotblocked cron should
@@ -31,4 +57,5 @@
   
   $config["hoursnotblocked_from_date"] = "";
   
-?>
+?> 
+
