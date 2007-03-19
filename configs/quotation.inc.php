85503dada412f63aca6b2c16bdf5f1f5db5eb9bf
Sandy Pleyte (SVN Import) <info@achievo.org>
2007-03-19 21:55:08 +0000

    Added config files for timereg, docmanager, quotation module

---
+++ configs/quotation.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+  // make the 'contactperson' field a relation instead of a textfield:
+  $config['contact_is_relation'] = false;
+
+  $config['quotation_dir'] = "./documents/quotations/";
+
+?>
