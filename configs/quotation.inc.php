c8adc0be3d5f24f8357fc0a9ca78ae735f79fc79
Sandy Pleyte (SVN Import) <info@achievo.org>
2007-03-20 07:24:42 +0000

    Added missing config: quotation_addproject

--- configs/quotation.inc.php
+++ configs/quotation.inc.php
@@ -3,6 +3,9 @@
   // make the 'contactperson' field a relation instead of a textfield:
   $config['contact_is_relation'] = false;
 
+  // Add project after quotation is accepted
+  $config['quotation_addproject'] = true;
+
   $config['quotation_dir'] = "./documents/quotations/";
 
 ?>
