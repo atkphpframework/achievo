5c481a8e77d0c904e2e948ce988ddd3e3f06f27a
Guido (SVN Import) <info@achievo.org>
2008-02-26 17:13:24 +0000

    + Added ability to configure projectcode autonumbering in the configuration   file configs/project.inc.php. [Guido]

--- configs/project.inc.php
+++ configs/project.inc.php
@@ -2,5 +2,22 @@
 
   // This variable configures wether hour registrations should be automatically deleted when
   // a phase is deleted. If omitted, false is default.
-  $config["project_cascading_delete_hours"] = false; 
+  $config["project_cascading_delete_hours"] = false;
+
+  // Set a prefix for your initial projectcodes
+  // Example: $config["projectcode_prefix"] = "[startdate.year][startdate.month]";
+  $config["projectcode_prefix"] = "";
+
+  // If you want an automatic incrementing number to be added to the projectcodes, set this to true
+  // Example: $config["projectcode_autonumber"] = true;
+  $config["projectcode_autonumber"] = false;
+
+  // Give the automatic incrementing a specific number of digits (the number will be prefixed by zero's)
+  // Example: $config["projectcode_autonumberdigits"] = 3;
+  $config["projectcode_autonumberdigits"] = 1;
+
+  // If you have a custom module that implements projectcodes, configure it's name here
+  // Example: $config["projectcode_module"] = "mymodule";
+  $config["projectcode_module"] = "";
+
 ?>
