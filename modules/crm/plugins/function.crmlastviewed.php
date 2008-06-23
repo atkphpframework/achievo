0b90e09bed60068ff30ea3f673cb2fe76638b69b
Sandy Pleyte (SVN Import) <info@achievo.org>
2008-06-23 20:41:40 +0000

    Updated code documentation

--- modules/crm/plugins/function.crmlastviewed.php
+++ modules/crm/plugins/function.crmlastviewed.php
@@ -1,5 +1,29 @@
 <?php
+/**
+ * This file is part of the Achievo distribution.
+ * Detailed copyright and licensing information can be found
+ * in the doc/COPYRIGHT and doc/LICENSE files which should be
+ * included in the distribution.
+ *
+ * @package achievo
+ * @subpackage crm
+ *
+ * @copyright (c)2008 Sandy Pleyte
+ * @copyright (c)2008 Ibuildings B.V.
+ * @license http://www.achievo.org/licensing Achievo Open Source License
+ *
+ * @version $Revision$
+ * $Id$
+ */
  
+  /**
+   * Class for managing activities
+   * 
+   * @author Sandy Pleyte <sandy@achievo.org>
+   * @package achievo
+   * @subpackage crm
+   * @since 1.3.0
+   */
   function smarty_function_crmlastviewed($params, &$smarty) 
   {
     $eventlog = &atkGetNode("crm.eventlog");
