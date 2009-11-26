be659f413ba35cfc820634f5e2bdd2402f4d92d3
Sandy Pleyte (SVN Import) <info@achievo.org>
2009-11-26 21:15:55 +0000

    Removed old unused config

--- configs/calendar.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  // This variable configures what the default status of a scheduled item should be.   
-  // if omitted, the default status is 'unconfirmed'.
-  $config["calendar_schedule_defaultstatus"] = "confirmed";  
-?>
