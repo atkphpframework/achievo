81faff839053d01dcd21b85b811537681b2ab222
klaaspeter <klaaspeter@live.nl>
2013-02-08 00:16:35 +0100

    Update configs/pim.inc.php
    Add example how to define the array to redirect the pim to an other module

--- configs/pim.inc.php
+++ configs/pim.inc.php
@@ -1,7 +1,10 @@
 <?php
 
 // default url to redirect to when the pim is called
-$config['pim_redirect_to']='';
+//example redirect to timereg module
+//$config['pim_redirect_to']= array('timereg.hours','admin');
+
+$config['pim_redirect_to']= array();
 
 // Array with disallowed pim items
 $config['disallowedpimitems']=array();
